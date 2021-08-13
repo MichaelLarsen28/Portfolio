cbf26914fb1f4eba6634bfc4a91ee513c5f534ed
MichaelLarsen28 <62678953+MichaelLarsen28@users.noreply.github.com>
2021-08-13 19:28:38 +1000

    changed linked to icons

--- src/Routes/HomePage/components/MenuBar.js
+++ src/Routes/HomePage/components/MenuBar.js
@@ -1,9 +1,11 @@
 import React from "react";
 import { withRouter } from "react-router-dom";
+import {IconContext} from "react-icons";
 import {Navbar, Container, Nav, NavDropdown} from "react-bootstrap";
 import Logo from "../../../Assets/Logo-5.png";
 import Resume from "../../../Assets/CV.pdf"
 import { AiFillGithub, AiFillLinkedin } from "react-icons/ai";
+import { MdEmail } from "react-icons/md";
 
 
 import "./MenuBar.css";
@@ -28,10 +30,21 @@ const MenuBar = (props) => {
       <Navbar.Toggle aria-controls="basic-navbar-nav" />
       <Navbar.Collapse className="justify-content-end">
         <Nav className="me-auto">
-          <Nav.Link href="mailto:michael.larsen@iinet.net.au">michael.larsen@iinet.net.au</Nav.Link>
-          <Nav.Link href="https://www.linkedin.com/in/michaellarsen28/" target="_blank"> <AiFillLinkedin/> </Nav.Link>
-          <Nav.Link href="https://github.com/MichaelLarsen28" target="_blank"> <AiFillGithub/> </Nav.Link>
-          <Nav.Link href={Resume} download="Michael_Larsen_CV">Resume</Nav.Link>
+          <Nav.Link href="mailto:michael.larsen@iinet.net.au">
+            <IconContext.Provider value={{ className:"myReact-icons"}}>
+            <MdEmail/>
+            </IconContext.Provider>
+          </Nav.Link>
+          <Nav.Link href="https://www.linkedin.com/in/michaellarsen28/" target="_blank">
+            <IconContext.Provider value={{ className:"myReact-icons"}}>
+            <AiFillLinkedin/>
+            </IconContext.Provider>
+           </Nav.Link>
+          <Nav.Link href="https://github.com/MichaelLarsen28" target="_blank">
+          <IconContext.Provider value={{ className:"myReact-icons"}}>
+          <AiFillGithub/>
+          </IconContext.Provider>
+          </Nav.Link>
         </Nav>
       </Navbar.Collapse>
       </Container>
