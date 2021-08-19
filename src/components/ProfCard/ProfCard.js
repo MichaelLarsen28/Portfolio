4525b3ab9de2cd816ad07547a4ae2bc738e432b2
MichaelLarsen28 <62678953+MichaelLarsen28@users.noreply.github.com>
2021-08-19 20:52:09 +1000

    proffessional section start

--- src/components/ProfCard/ProfCard.js
+++ src/components/ProfCard/ProfCard.js
@@ -18,7 +18,18 @@ const ProfCard = (props) => {
           <p><b>01.</b>&nbsp;Siemens Energy</p>
 
           <p>
-          blah blah blah
+          Most of my internship at Siemens Energy was spent at a power station
+          where I was a part of the commissioning team.  The team was responsible
+          for testing signals for the newly upgraded distributed control system.
+          My primary role was desk technician where I made sure that signals,
+          alarms, and faults on the HMI (human-machine interface) displayed
+          correctly when triggered by a field technician.  I also helped the
+          network engineering team set-up and test the multiple servers and
+          clients to allow the control system to receive field data.  I moved
+          into the return to service team when the commissioning team completed
+          all signal tests.  This is where I communicated regularly with the
+          customer and participated in multiple meetings to get the plant
+          running with the new control system.
           </p>
 
           <p><b>02.</b>&nbsp;OneWiFi Australia</p>
